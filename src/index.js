4b63762cd9b6b298a8c11d95f694b88fb8128b9c
marvin espira <espiramarvin@gmail.com>
2022-08-17 11:32:26 +0300

    adds react-query

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
