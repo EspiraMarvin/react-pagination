9cb288c79478246a049032973e88bd3fb534e89c
marvin espira <espiramarvin@gmail.com>
2022-08-18 17:52:43 +0300

    example3 import updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Example1 from './Example1'
 import Example2 from './Example2'
+import Example3 from './Example3'
 
 
 function App() {
-  return <Example2 />
+  return <Example3 />
 }
 
 export default App;
